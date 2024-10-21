05956b341bad0ffdb2b42b573de32b70f1d16c41
Ashton Snelgrove <ashton@snelgrove.science>
2024-10-21 09:39:44 -0600

    Set mode for pins in irq file.

--- src/gpio_interrupt.c
+++ src/gpio_interrupt.c
@@ -13,10 +13,14 @@ void irq_callback(uint gpio, uint32_t event_mask)
 int main(void)
 {
     stdio_init_all();
+
     gpio_init(IN_PIN);
+    gpio_set_dir(IN_PIN, GPIO_IN);
+
     gpio_init(OUT_PIN);
+    gpio_set_dir(OUT_PIN, GPIO_OUT);
     gpio_put(OUT_PIN, toggle);
-    gpio_set_input_enabled(IN_PIN, true);
+
     gpio_set_irq_enabled_with_callback(IN_PIN, GPIO_IRQ_EDGE_RISE, true, irq_callback);
     while(1) __wfi();
     return 0;
